955e2a39e072bd1b6bb04fef5b06ea028108e3a0
jakubkrhovjak <c_jkrhovjak@groupon.com>
2025-11-22 15:20:45 +0100

    init

--- internal/student/model.go
+++ internal/student/model.go
@@ -6,8 +6,8 @@ type Student struct {
 	bun.BaseModel `bun:"table:students,alias:s"`
 
 	ID        int    `bun:"id,pk,autoincrement" json:"id"`
-	FirstName string `bun:"first_name,notnull" json:"firstName" validate: "required"`
-	LastName  string `bun:"last_name,notnull" json:"lastName" validate: "required"`
+	FirstName string `bun:"first_name,notnull" json:"firstName" validate:"required"`
+	LastName  string `bun:"last_name,notnull" json:"lastName" validate:"required"`
 	Email     string `bun:"email,unique,notnull" json:"email" validate:"required,email"`
 	Major     string `bun:"major" json:"major"`
 	Year      int    `bun:"year" json:"year" validate:"min=0,max=10"`
